b4688d1f0ac8a3dca3e14e5c5accb157f3364e82
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-05-20 09:33:34 -0700

    finish hook

--- src/hooks/useGallery.js
+++ src/hooks/useGallery.js
@@ -1,6 +1,17 @@
-import react, {useState, useEffect} from 'react'
+import react, {useState, useEffect, withRouter} from 'react'
+import { useParams } from 'react-router'
 import fetchCharacters from '../services/avatarApi'
 
-const useGallery = (id = "") => {
+const useGallery = () => {
+  const [characters, setCharacters] = useState()
+  const [loading, setLoading] = useState(true)
+  const { id } = useParams() || ""
 
+  useEffect(() => {
+    const characters =  fetchCharacters(id)
+      .then(characters => setCharacters(characters))
+      .finally(() => setLoading(false))
+  }, [])
+
+  return {characters, loading}
 }
